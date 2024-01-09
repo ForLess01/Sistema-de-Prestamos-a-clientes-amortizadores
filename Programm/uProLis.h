49da6e2c9036d202ec66e896e938dd349b64b191
cjelgamer <119980335+cjelgamer@users.noreply.github.com>
2024-01-08 19:33:51 -0500

    Create uProLis.h

---
+++ Programm/uProLis.h
@@ -0,0 +1,11 @@
+#if!defined_uProLis_h_
+#define_uProLis_h_
+//definici�n de la clase
+class cProLis
+{
+public :
+    virtual void Ejecuta();
+    virtual int Menu();
+    virtual void RealizaAcciones(int pvOpcion);
+};
+#endif
